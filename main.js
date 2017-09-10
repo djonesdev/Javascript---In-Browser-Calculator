9c3f2a2a5e0ce14b62812287616182c11968ebc0
djonesdev <31727436+djonesdev@users.noreply.github.com>
2017-09-10 17:38:57 +0100

    Add files via upload
    Added power and square root, also tried new error messages

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
   var num1,num2,ans,oper;
   num1=prompt("Type any number");
-  oper=prompt("Type an operator (+ = addition, - = subtraction, * = mutiplication, / = divide");
+  oper=prompt("Type an operator (+ = addition, - = subtraction, * = mutiplication, / = divide, ^ = power, or sqrt for the square root (Note: a second number is not needed for this function).");
   num2=prompt("Type another number");
-  if(oper=="+"){
+  if(oper==="+"){
     ans=parseFloat(num1)+parseFloat(num2);
   }
-  else if(oper=="/"){
+  else if(oper==="/"){
     ans=parseFloat(num1)/parseFloat(num2);
   }
-  else if(oper=="*"){
+  else if(oper==="*"){
     ans=parseFloat(num1)*parseFloat(num2);
   }
-  else if(oper=="-"){
+  else if(oper==="-"){
     ans=parseFloat(num1)-parseFloat(num2);
   }
-    else if(oper=!null){
-    ans="Error"
+  else if(oper==="^"){
+    ans=Math.pow(num1,num2);
+  }
+  else if (oper==="sqrt"){
+    ans=Math.sqrt(num1);
+  }
+  else if(oper=!null){
+    ans="Error: Please enter a operator."
   }
   else if(num1=!null){
-    ans="Error"
+    ans="Error: Please enter a number."
   }
   else if(num2=!null){
-    ans="Error"
+    ans="Error: Please enter a number."
   }
-  alert(ans);
+  alert('The answer is ' + ans);
